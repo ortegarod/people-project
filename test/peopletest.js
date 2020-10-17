9fcccb3d0b0b3121f2f22bb77d015c1b195c7931
Rodrigo Ortega <ortegarod000@gmail.com>
2020-10-17 13:43:01 -0500

    updated truffle-assertions.fails statement

--- test/peopletest.js
+++ test/peopletest.js
@@ -5,6 +5,7 @@ contract("People", async function(){
     it("shouldn't create a person with age over 150 years", async function(){
     let instance = await People.deployed();
     // tests a require statement using truffle-assertions
-    await TruffleAssert.fails(instance.createPerson("Bob", 200, 190, {value: web3.utils.toWei("1", "ether")}));
+    // will return "AssertionError: Did not fail" if test fails
+    await TruffleAssert.fails(instance.createPerson("Bob", 200, 190, {value: web3.utils.toWei("1", "ether")}), TruffleAssert.ErrorType.REVERT);
     })
 });
