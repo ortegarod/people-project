5e8b540b1ad0d404695afed7fe897eefac46c459
Rodrigo Ortega <ortegarod000@gmail.com>
2020-10-17 13:37:44 -0500

    added test to test require statement

---
+++ test/peopletest.js
@@ -0,0 +1,10 @@
+const People = artifacts.require("People");
+const TruffleAssert = require("truffle-assertions");
+
+contract("People", async function(){
+    it("shouldn't create a person with age over 150 years", async function(){
+    let instance = await People.deployed();
+    // tests a require statement using truffle-assertions
+    await TruffleAssert.fails(instance.createPerson("Bob", 200, 190, {value: web3.utils.toWei("1", "ether")}));
+    })
+});
