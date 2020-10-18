fd323ed0ece26faa3f5eb4b69efeb3559afc68bd
Rodrigo Ortega <ortegarod000@gmail.com>
2020-10-18 15:02:59 -0500

    added several new tests for people contract

--- test/peopletest.js
+++ test/peopletest.js
@@ -1,4 +1,5 @@
 const People = artifacts.require("People");
+const AssertionError = require("assertion-error");
 const TruffleAssert = require("truffle-assertions");
 
 contract("People", async function(){
@@ -7,5 +8,21 @@ contract("People", async function(){
     // tests a require statement using truffle-assertions
     // will return "AssertionError: Did not fail" if test fails
     await TruffleAssert.fails(instance.createPerson("Bob", 200, 190, {value: web3.utils.toWei("1", "ether")}), TruffleAssert.ErrorType.REVERT);
+    })
+    it("shouldn't create a person without payment", async function(){
+        let instance = await People.deployed();
+        await TruffleAssert.fails(instance.createPerson("Bob", 50, 190, {value: web3.utils.toWei("0.5", "ether")}), TruffleAssert.ErrorType.REVERT);
+    })
+    it("should set senior status correctly", async function(){
+        let instance = await People.deployed();
+        await instance.createPerson("Bob", 65, 190, {value: web3.utils.toWei("1", "ether")});
+        let result = await instance.getPerson();
+        assert(result.senior === true, "senior level not set");
+        // can also combine "assert(result.senior === true && ..., "...");""
+    })
+    it("should set age correctly", async function(){
+        let instance = await People.deployed();
+        let result = await instance.getPerson();
+        assert(result.age.toNumber() === 65, "age not set correctly");
     })
 });
